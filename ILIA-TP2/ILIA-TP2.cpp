6e5e02f32947e57695861376fc2c82ca008d60e7
Maxence Locufier <malocufier@gmail.com>
2021-03-26 14:25:58 +0100

    Première étape

--- ILIA-TP2/ILIA-TP2.cpp
+++ ILIA-TP2/ILIA-TP2.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+const int nMax = 10;
+
 int main()
 {
+  int n;
+  do {
+    cout << "Entrez un nombre entre 1 et " << nMax << ": ";
+    cin >> n;
+  } while (n < 1 || n > nMax);
+
   return 0;
 }
