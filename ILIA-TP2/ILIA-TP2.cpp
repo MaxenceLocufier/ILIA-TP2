c72a471d7e9c63356a867971fe637e20e2c7f988
Maxence Locufier <malocufier@gmail.com>
2021-03-26 16:17:09 +0100

    Troisième étape

--- ILIA-TP2/ILIA-TP2.cpp
+++ ILIA-TP2/ILIA-TP2.cpp
@@ -3,10 +3,25 @@ using namespace std;
 
 const int nMax = 10;
 
-void sum(int n);
-void avr(int n);
-void max(int n);
-void min(int n);
+void sum(int);
+void avr(int);
+void max(int);
+void min(int);
+
+void fill_array(double[], int);
+void fill_array(int[], int);
+void fill_array(string[], int);
+void fill_array(char[][nMax], int);
+
+void print_array(double[], int);
+void print_array(int[], int);
+void print_array(string[], int);
+void print_array(char[][nMax], int);
+
+double sum_array(double[], int);
+double average_array(int[], int);
+string max_array(string[], int);
+char min_array(char[][nMax], int);
 
 int main()
 {
@@ -44,18 +59,132 @@ int main()
   return 0;
 }
 
+#pragma region Operations
+
 void sum(int n) {
-  cout << "Somme";
+  double tab[nMax];
+  fill_array(tab, n);
+  print_array(tab, n);
+  cout << "Somme: " << sum_array(tab, n) << endl;
 }
 
 void avr(int n) {
-  cout << "Moyenne";
+  int tab[nMax];
+  fill_array(tab, n);
+  print_array(tab, n);
+  cout << "Moyenne: " << average_array(tab, n) << endl;
 }
 
 void max(int n) {
-  cout << "Maximum";
+  string tab[nMax];
+  fill_array(tab, n);
+  print_array(tab, n);
+  cout << "Maximum: " << max_array(tab, n) << endl;
 }
 
 void min(int n) {
-  cout << "Minimum";
+  char mat[nMax][nMax];
+  fill_array(mat, n);
+  print_array(mat, n);
+  cout << "Minimum: '" << min_array(mat, n) << "'\n";
 }
+
+#pragma endregion
+
+
+#pragma region ArrayOperations
+
+void fill_array(double tab[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << "Nombre " << i << ": ";
+    cin >> tab[i];
+  }
+}
+
+void fill_array(int tab[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << "Nombre " << i << ": ";
+    cin >> tab[i];
+  }
+}
+
+void fill_array(string tab[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << "Texte " << i << ": ";
+    cin >> tab[i];
+  }
+}
+
+void fill_array(char mat[][nMax], int n) {
+  string s;
+  for (int i = 0; i < n; i++) {
+    do {
+      cout << "Ligne " << i << ": ";
+      cin >> s;
+    } while (s.length() != n);
+    strncpy_s(mat[i],s.c_str(), n);
+  }
+}
+
+void print_array(double tab[], int n) {
+  cout << "[";
+  for (int i = 0; i < n; i++) {
+    cout << tab[i] << (i == n - 1 ? "]\n" : ", ");
+  }
+}
+
+void print_array(int tab[], int n) {
+  cout << "[";
+  for (int i = 0; i < n; i++) {
+    cout << tab[i] << (i == n - 1 ? "]\n" : ", ");
+  }
+}
+
+void print_array(string tab[], int n) {
+  cout << "[";
+  for (int i = 0; i < n; i++) {
+    cout << tab[i] << (i == n - 1 ? "]\n" : ", ");
+  }
+}
+
+void print_array(char tab[][nMax], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++)
+      cout << tab[i][j] << " ";
+    cout << endl;
+  }
+}
+
+double sum_array(double tab[], int n) {
+  double s = 0;
+  for (int i = 0; i < n; i++) {
+    s += tab[i];
+  }
+  return s;
+}
+
+double average_array(int tab[], int n) {
+  double s = 0;
+  for (int i = 0; i < n; i++) {
+    s += tab[i];
+  }
+  return s/n;
+}
+
+string max_array(string tab[], int n) {
+  string max = tab[0];
+  for (int i = 1; i < n; i++) {
+    if (max < tab[i]) max = tab[i];
+  }
+  return max;
+}
+
+char min_array(char tab[][nMax], int n) {
+  char min = tab[0][0];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (tab[i][j] < min) min = tab[i][j];
+  return min;
+}
+
+#pragma endregion
